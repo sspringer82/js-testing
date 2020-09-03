be9df175403bd1187558d80d38197888566812c6
Sebastian Springer <info@sebastian-springer.com>
2020-09-03 10:31:33 +0200

    tdd

--- spec/calc.spec.js
+++ spec/calc.spec.js
@@ -1,3 +1,28 @@
-it('should work', () => {
-  expect(true).toBe(true);
+describe('Calculator', () => {
+  let calc;
+  beforeAll(() => {});
+  beforeEach(() => {
+    // Arrange
+    calc = new Calculator();
+  });
+  afterEach(() => {});
+  afterAll(() => {});
+
+  it('should add 1 and 1 and return 2', () => {
+    // tripple A
+    // Arrange
+    // Act
+    const result = calc.add(1, 1);
+    // Assert
+    expect(result).toBe(2);
+  });
+
+  //triangulate
+  it('should add 2 and 2 and return 4', () => {
+    // tripple A
+    // Act
+    const result = calc.add(2, 2);
+    // Assert
+    expect(result).toBe(4);
+  });
 });
---
+++ src/calc.js
@@ -0,0 +1,5 @@
+class Calculator {
+  add(a, b) {
+    return a + b;
+  }
+}
