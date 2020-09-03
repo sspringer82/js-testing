0d7087afca7ee39e83fe3b59fbd7624ba72e28f2
Sebastian Springer <info@sebastian-springer.com>
2020-09-03 11:12:37 +0200

    subdescribe

--- spec/calc.spec.js
+++ spec/calc.spec.js
@@ -8,21 +8,23 @@ describe('Calculator', () => {
   afterEach(() => {});
   afterAll(() => {});
 
-  it('should add 1 and 1 and return 2', () => {
-    // tripple A
-    // Arrange
-    // Act
-    const result = calc.add(1, 1);
-    // Assert
-    expect(result).toBe(2);
-  });
+  describe('add', () => {
+    it('should add 1 and 1 and return 2', () => {
+      // tripple A
+      // Arrange
+      // Act
+      const result = calc.add(1, 1);
+      // Assert
+      expect(result).toBe(2);
+    });
 
-  //triangulate
-  it('should add 2 and 2 and return 4', () => {
-    // tripple A
-    // Act
-    const result = calc.add(2, 2);
-    // Assert
-    expect(result).toBe(4);
+    //triangulate
+    it('should add 2 and 2 and return 4', () => {
+      // tripple A
+      // Act
+      const result = calc.add(2, 2);
+      // Assert
+      expect(result).toBe(4);
+    });
   });
 });
